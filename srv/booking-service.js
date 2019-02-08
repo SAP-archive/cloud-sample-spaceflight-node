5fc38ad6a1c813fe6a5eb134e69d0acefe0c5b44
Christoph Pohl <christoph.pohl@sap.com>
2019-02-08 16:59:47 +0100

    Upgrade to sap/cds 3 (#3)
    - Config changes for dependencies and build
    - Migrate custom code to sap/cds 3 APIs

--- srv/booking-service.js
+++ srv/booking-service.js
@@ -2,22 +2,21 @@
  * Custom logic for booking-service defined in ./booking-service.cds
  * Check to restric number of passenger traveling on a space craft to 5
  */
-module.exports = function ({ teched_flight_trip_Bookings }) {
+module.exports = (srv) => {
 
-    this.before ('CREATE', teched_flight_trip_Bookings, (cds) => {
-  
-      cds.run(()=>{
-         SELECT.from ('teched_flight_trip_Bookings')
-          .where ({DateOfTravel: cds.data.DateOfTravel, and : {Itinerary_ID: cds.data.Itinerary_ID} })
-        
-      }).then(( [bookings] ) => {
-          let totalPassengers = 0
-          for (let booking of bookings) {
-            totalPassengers = totalPassengers + booking.NumberOfPassengers
-            if (totalPassengers + cds.data.NumberOfPassengers >= 5)
-              cds.error (409, "Spacecraft tickets sold out for your Date/Destination, sorry!")
-          }
-      })
-      
+    const {Bookings} = cds.entities('teched.flight.trip')
+
+    srv.before ('CREATE', 'Bookings', async (req) => {
+      const {DateOfTravel, Itinerary_ID, NumberOfPassengers} = req.data
+      const tx = cds.transaction(req)
+      const bookings = await tx.run(
+         SELECT.from (Bookings).where ({ DateOfTravel, /*and*/ Itinerary_ID })
+      )
+      let totalPassengers = 0
+      bookings.forEach(booking => { totalPassengers += booking.NumberOfPassengers })
+      if (totalPassengers + NumberOfPassengers >= 5) {
+        return req.error (409, "Spacecraft tickets sold out for your Date/Destination, sorry!")
+      }
+
     })
   }
